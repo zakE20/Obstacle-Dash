dbb5ad69932f5994cf2505a1ad908dfa7123f173
Zakaria Laachiri <zakarialaachiri2@gmail.com>
2025-02-07 00:54:21 +0100

    feat:generating a canvas configuration

---
+++ index.js
@@ -0,0 +1,7 @@
+let canvas=document.getElementById("gameCanvas");
+let ctx=canvas.getContext("2d");
+
+canvas.style.backgroundColor="lightblue";
+canvas.style.border="1px solid black";
+canvas.style.margin="auto";
+canvas.style.display="flex";
