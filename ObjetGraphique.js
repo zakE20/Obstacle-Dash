d93918d3ba898f13aa09fc6a19ba1310cf779d96
Zakaria Laachiri <zakarialaachiri2@gmail.com>
2025-02-07 18:56:28 +0100

    feat:adding a class of object

---
+++ ObjetGraphique.js
@@ -0,0 +1,14 @@
+class ObjetGraphique {
+    constructor(x, y, width, height, color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
